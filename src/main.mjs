17d27423974605b3e50da26770e8c979de712344
Hans-Peter Gygax <gygax@practicomp.ch>
2026-08-23 21:34:07 +0200

    Remove light/dark mode override (for now)

--- src/main.mjs
+++ src/main.mjs
@@ -11,12 +11,13 @@ import App from './App.vue';
 import routes from './router';
 
 // Select / set the initial theme
-const THEME_STORAGE_KEY = 'dm-screen-theme';
-const VALID_THEMES = new Set(['light', 'dark', 'auto']);
-const storedTheme = localStorage.getItem(THEME_STORAGE_KEY);
-const initialTheme = VALID_THEMES.has(storedTheme) ? storedTheme : 'light';
-document.documentElement.dataset.theme = initialTheme;
-document.documentElement.style.colorScheme = initialTheme === 'dark' ? 'dark' : 'light';
+// const THEME_STORAGE_KEY = 'dm-screen-theme';
+// const VALID_THEMES = new Set(['light', 'dark', 'auto']);
+// const storedTheme = localStorage.getItem(THEME_STORAGE_KEY);
+// const initialTheme = VALID_THEMES.has(storedTheme) ? storedTheme : 'light';
+// document.documentElement.dataset.theme = initialTheme;
+// document.documentElement.style.colorScheme = initialTheme === 'dark' ? 'dark' : 'light';
+// console.log(`Initial theme: ${initialTheme}`);
 
 const router = createRouter({
   history: createWebHistory(),
@@ -32,7 +33,7 @@ const app = createApp(App)
       preset: Aura,
       options: {
         prefix: 'p',
-        darkModeSelector: '[data-theme="dark"]',
+        darkModeSelector: 'system', //'[data-theme="dark"]',
         cssLayer: false
       }
     }
